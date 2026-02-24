b073480c09c1991cd7a41fb8b0b0394603052881
Flozzyozzy <floremarsh@gmail.com>
2026-02-24 10:23:33 +0300

    First commit

---
+++ test.py
@@ -0,0 +1,25 @@
+# importing flask to our application
+from flask import *
+
+# initializing the flask app
+app = Flask(__name__)
+
+# define a simple route/endpoint
+# this route corresponds to a web application function
+@app.route("/api/home")
+# this is the function that runs when the route is accessed
+def home():
+    return jsonify({"Message":"Welcome to home api"})
+
+
+
+
+# define a simple route
+@app.route("/api/products")
+# function corresponding to the route
+def products():
+    return jsonify ({"Message": "Welcome to products api"})
+
+
+# runs the app when this file is executed
+app.run(debug = True)
